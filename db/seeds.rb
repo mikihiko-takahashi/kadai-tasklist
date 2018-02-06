81a3afb28c2218e69d4d261ecca1ace33660b8de
mikihiko.takahashi <mikihiko.takahashi@gmail.com>
2018-02-06 14:46:51 +0000

    kadai-tasklist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#(1..100).each do |number|
+#  Task.create(status: 'test status ' + number.to_s, content: 'test task ' + number.to_s)
+#end
+
+Task.create(status: 'test status 1', content: 'test task 1')
+Task.create(status: 'test status 2', content: 'test task 2')
+Task.create(status: 'test status 3', content: 'test task 3')
